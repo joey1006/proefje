4586731386ff7fa682fcf0e01c95049e42e8b9e3
joey steffens <joeysteffens@quicknet.nl>
2019-02-28 14:59:54 +0100

    Gallery start

--- view/head.php
+++ view/head.php
@@ -1,6 +1,6 @@
 <head>
 	<title>Stumpel showcase</title>
 
-	<link href="css/style.css">
-	<script src="javascript/javascript.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="../javascript/javascript.js"></script>
 </head>
