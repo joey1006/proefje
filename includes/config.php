b3ff38523b9d07adcda551a3b7132080e05029bf
basv1996 <bas.vugts@gmail.com>
2019-03-06 21:00:11 +0100

    Database connectie

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Define project status
+define('PROJECT_STATUS','development');
+
+setlocale(LC_ALL, 'nl_NL');
+
+// Database settings
+define('DB_HOST','localhost');
+define('DB_NAME','books');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+
+?>
