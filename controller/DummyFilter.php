c970a6c9a833ea934d06d7d6efdfa9c43e548f1c
basv1996 <bas.vugts@gmail.com>
2019-03-12 14:07:24 +0100

    filter

--- controller/DummyFilter.php
+++ controller/DummyFilter.php
@@ -1,5 +1,4 @@
 <?php
-
 if(isset($_POST['search']))
 {
     $valueToSearch = $_POST['valueToSearch'];
@@ -22,43 +21,3 @@ function filterTable($query)
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Filteren</title>
-        <style>
-            table,tr,th,td
-            {
-                border: 1px solid black;
-            }
-        </style>
-    </head>
-    <body>
-        
-        <form action="DummyFilter.php" method="post">
-            <input type="text" name="valueToSearch" placeholder="Zoek..."><br><br>
-            <input type="submit" name="search" value="Filter"><br>
-            <input type="submit" name="reset" value="reset"><br><br>
-            
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Boektitel</th>
-                    <th>Categorie</th>
-                    <th>Auteur</th>
-                </tr>
-
-                <?php while($row = mysqli_fetch_array($search_result)):?>
-                <tr>
-                    <td><?php echo $row['Id'];?></td>
-                    <td><?php echo $row['Title'];?></td>
-                    <td><?php echo $row['Category'];?></td>
-                    <td><?php echo $row['Author'];?></td>
-                </tr>
-                <?php endwhile;?>
-            </table>
-        </form>
-        
-    </body>
-</html>
