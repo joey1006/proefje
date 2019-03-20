c34d2466649853817d614785083bbc2565a7370a
joey steffens <joeysteffens@quicknet.nl>
2019-03-20 11:28:11 +0100

    bass php file pushen

---
+++ view/preview_books.php
@@ -0,0 +1,39 @@
+<div class="Preview_books">
+  <div class="book">
+   <div class="Book_cover_front">
+      <p class="BookTitle">titel</p>
+      <p class="jouw_naam">Jouw naam</p>
+      <div class="sticker"></div>
+   </div>
+   <div class="Book_cover_back">
+      <img src="#" />
+      <p class="description"> </p>
+      <div class="sticker"></div>
+      </div>
+   </div>
+   <div class="book">
+   <div class="Book_cover_front">
+      <p class="BookTitle">titel</p>
+      <p class="jouw_naam">Jouw naam</p>
+      <div class="sticker"></div>
+   </div>
+   <div class="Book_cover_back">
+      <img src="#"/>
+      <p class="description"></p>
+      <div class="sticker"></div>
+      </div>
+   </div>
+   <div class="book">
+   <div class="Book_cover_front">
+      <p class="BookTitle">titel</p>
+      <p class="jouw_naam">Jouw naam</p>
+      <div class="sticker"></div>
+   </div>
+   <div class="Book_cover_back">
+      <img src="#"/>
+      <p class="description"></p>
+      <div class="sticker"></div>
+      </div>
+   </div>
+   
+</div>
